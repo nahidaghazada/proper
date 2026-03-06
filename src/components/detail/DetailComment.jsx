9011429ac7ac2fcd5f6a41ad31e8a34bebe0e99a
nahidaghazada <agazada.nahid@gmail.com>
2026-03-06 16:14:45 +0400

    add api comments

--- src/components/detail/DetailComment.jsx
+++ src/components/detail/DetailComment.jsx
@@ -5,7 +5,6 @@ import toast from "react-hot-toast"
 function DetailComment({ productId }) {
     const [content, setContent] = useState("")
     const [page, setPage] = useState(1)
-
     const { data } = useGetCommentsQuery({ productId, page })
     const [createComment, { isLoading: isAdding }] = useCreateCommentMutation()
 
@@ -31,10 +30,7 @@ function DetailComment({ productId }) {
                 <button onClick={handleAddComment} disabled={isAdding}
                     className="bg-black cursor-pointer text-[#fff] px-8 py-2 rounded-full text-sm font-bold hover:bg-gray-800 disabled:opacity-60 flex items-center gap-2">
                     {isAdding && (
-                        <svg className="animate-spin h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
-                        </svg>
+                        <svg className="animate-spin h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" /><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" /></svg>
                     )}
                     {isAdding ? "Adding..." : "Add"}
                 </button>
